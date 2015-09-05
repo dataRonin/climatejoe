1b0a99460dcd0280b5725a8bb90263964935cb82
FOX PETERSON <fox@tinybike.net>
2015-09-05 14:39:25 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'VtPIYssEUOyd8P4xJg093p31w',
+  consumer_secret:      'qwJwHgmXMob7XQkid5VmY0ad3Ysj7ZkabrbwDSUTLEp2RwTVfU',
+  access_token:         '3463377673-b5VeDHo8VxFMWZQ9CrlFYHfGEmUu9YGjT1XXemu',
+  access_token_secret:  'NxVBEdsLvMCb4ToFwkmIQJHgBU2PGhQrbEXRcirlZMpaH'
+}
