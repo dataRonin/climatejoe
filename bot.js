c1b0cfbf1392bca95c4831662e8d1f8566fe443c
FOX PETERSON <fox@tinybike.net>
2015-09-05 15:33:42 -0700

    bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ function retweetLatest() {
 	  if (!error) {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
+		var retweetText = data.statuses[0].text;
 		// ...and then we tell Twitter we want to retweet it!
 		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
 			if (response) {
@@ -36,6 +37,7 @@ function retweetLatest() {
 
 // Try to retweet something as soon as we run the program...
 retweetLatest();
+
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
